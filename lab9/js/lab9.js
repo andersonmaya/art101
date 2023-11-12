c149276b03576de58c6c84dcfadd62da5bc8e702
andersonmaya <mayaanderson3300@gmail.com>
2023-11-11 18:33:49 -0800

    pushing
    yes

--- lab9/js/lab9.js
+++ lab9/js/lab9.js
@@ -10,8 +10,11 @@
 // add button to challenge section
 $("#challenge").append("<button id='button-challenge'>Make Special</button>");
 
+slideElement = $("#challenge");
+
 // add a click listener to the challenge button
 $("button-challenge").click(function(){
     // now add (or subtract) the "special" class to the section
     $("#challenge").toggleClass("special");
 });
+
