11a4eb03ea9c0731762de43939a69eb3b8e3aac5
andersonmaya <mayaanderson3300@gmail.com>
2023-11-16 20:27:45 -0800

    button
    orange

--- lab10/js/lab10.js
+++ lab10/js/lab10.js
@@ -1,8 +1,7 @@
 /*
-   lab.js - This simple JavaScript/jQuery script uses buttons to modify some elements on the page
-
+   lab.js 
+   This simple JavaScript/jQuery script uses buttons to modify some elements on the page
    Requirements: jQuery must be loaded for this script to work.
-
    Author: Maya Anderson
    Date: 2023
 */
